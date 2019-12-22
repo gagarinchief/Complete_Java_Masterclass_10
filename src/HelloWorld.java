fe454a35328f97241273c1370b11197750ad3639
Perfect User <me@PerfectUser.com>
2019-12-23 02:17:11 +0300

    First Variables

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World and You!");
+
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        int mySecondNumber = (5 + 10) + (2 * 10);
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+        System.out.println("mySecondNumber");
+    }
+}
