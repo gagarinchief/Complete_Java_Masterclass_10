b97627e2a920b27a12be1e7b230c33023bd4523f
Perfect User <me@PerfectUser.com>
2019-12-23 05:04:46 +0300

    FloatDouble and PoundsToKilos

--- src/ByteShortInt.java
+++ src/ByteShortInt.java
@@ -1,24 +1,24 @@
 public class ByteShortInt {
     public static void main(String[] args) {
 
-        // int has a width of 32
+        // int has a width of 32 (4 bytes)
         int myMinValue = -2_147_483_648;
         int myMaxValue = 2_147_483_647;
         int myTotal = (myMinValue/2);
         System.out.println("myTotal = " + myTotal);
 
-        // byte has a width of 8
+        // byte has a width of 8 (1 byte)
         byte byteMaxValue = 127;
         byte byteMinValue = -128;
         byte myNewByteValue = (byte) (myMinValue/2);
         System.out.println("myNewByteValue = " + myNewByteValue);
 
-        // short has a width of 16
+        // short has a width of 16 (2 bytes)
         short shortMaxValue = 32767;
         short shortMinValue = -32768;
         short myNewShortValue = (short) (shortMaxValue / 2);
 
-        // long has a width of 64
+        // long has a width of 64 (8 bytes)
         long longMaxValue = 9_223_372_036_854_775_807L;
         long longMinValue = -9_223_372_036_854_775_808L;
         long myLongValue = 50_000L;
---
+++ src/PoundsToKilos.java
@@ -0,0 +1,8 @@
+public class PoundsToKilos {
+    public static void main(String[] args) {
+        double numPounds = 200d;
+        double pound = 0.45359237d;
+        double convertedKilos = numPounds * pound;
+        System.out.println(convertedKilos);
+    }
+}
