82df85e7543acf4dbf5908373724b0acf75499f4
Perfect User <me@PerfectUser.com>
2019-12-23 21:42:07 +0300

    CharAndBoolean

---
+++ src/CharAndBoolean.java
@@ -0,0 +1,10 @@
+public class CharAndBoolean {
+    public static void main(String[] args) {
+        // width of 16 (2 bytes)
+        char myChar = '\u00AE'; // хранит одну букву или символ
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = true;
+        boolean isMale = true;
+    }
+}
