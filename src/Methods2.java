6a8e58edbb1c205a0efc1cb0c4f143454a93bad9
Perfect User <me@PerfectUser.com>
2020-01-04 17:25:57 -0500

    Перегрузка метода (кастинг) урок 19

--- src/Methods2.java
+++ src/Methods2.java
@@ -33,35 +33,41 @@ public class Methods2 {
 
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-         if (gameOver) {
-             int finalScore = score + (levelCompleted + bonus);
-             finalScore += 2000;
-             return finalScore;
-         }
-         return -1;
+        if (gameOver) {
+            int finalScore = score + (levelCompleted + bonus);
+            finalScore += 2000;
+            return finalScore;
+        }
+        return -1;
 
 
+    }
 
-
-}
-
-    public static void displayHighScorePosition(String playerName, int highScorePosition){
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
         System.out.println(playerName + " managed to get into position " + highScorePosition
                 + " on the high score table");
     }
 
-    public static int calculateHighScorePosition(int playerScore){
-        if (playerScore >= 1000) {
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
-            return 3;
-        } else {
-            return 4;
-        }
-    }
+    public static int calculateHighScorePosition(int playerScore) {
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        }
+//            return 4;
+        int position = 4; //assuming position 4 will be returned
 
+        if (playerScore >= 1000) {
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+        }
+        return position;
     }
+}
+
 
 
