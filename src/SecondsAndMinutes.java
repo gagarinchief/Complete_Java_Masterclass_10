76f6fff6ccfc3206ce8a52ade224de81019131ea
Perfect User <me@PerfectUser.com>
2020-01-04 21:52:09 -0500

    Добавлена константа в секунды и минуты

--- src/SecondsAndMinutes.java
+++ src/SecondsAndMinutes.java
@@ -1,17 +1,39 @@
 public class SecondsAndMinutes {
+
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value"; //Создана константа (заглавными буквами), типа final, т.к.значение переменной нельзя изменять.
+
     public static void main(String[] args) {
-        System.out.println(getDurationString(65, 47));
+        System.out.println(getDurationString(85, 45));
         System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-41));
+        System.out.println(getDurationString(65, 9));
 
     }
 
     public static String getDurationString(int minutes, int seconds) {
-        if ((seconds < 0) || (seconds > 59) || (minutes < 0)) {
-            return "Invalid value";
+        if ((minutes < 0) || (seconds < 0) || (seconds > 59) ) {
+            return INVALID_VALUE_MESSAGE;
         }
         int hours = minutes / 60;
         int remainingMinutes = minutes % 60;
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        String hoursString = hours + "h";
+        if (hours < 10) {
+            hoursString = "0" + hoursString;
+        }
+
+        String minutesString = remainingMinutes + "m";
+        if (remainingMinutes < 10) {
+            minutesString = "0" + minutesString;
+        }
+
+        String secondsString = seconds + "s";
+        if (seconds < 10) {
+            secondsString = "0" + secondsString;
+        }
+
+//                return hours + "h " + remainingMinutes + "m " + seconds + "s";
+                return hoursString + " " + minutesString + " " + secondsString + "";
     }
 
     public static String getDurationString(int seconds) {
